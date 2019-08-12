2d918128bf6831fa83bbe5db96a911e1ad717a89
bruce.wang <www6v@126.com>
2019-08-12 16:57:07 +0800

    count

--- src/main/scala/com/flink/common/richf/RtcMonitorInitRichFlatMapFunction.scala
+++ src/main/scala/com/flink/common/richf/RtcMonitorInitRichFlatMapFunction.scala
@@ -8,53 +8,43 @@ import org.apache.flink.configuration.Configuration
 import org.apache.flink.util.Collector;
 
 class RtcMonitorInitRichFlatMapFunction
-  extends RichFlatMapFunction[MonitorRoomBean, (String,  String, Long, Integer)]
+  extends RichFlatMapFunction[MonitorRoomBean, (String,  String, Long, Integer,Integer)]
 {
-  private var userCount: ValueState[Integer] = _
+  private var currentUserCount: ValueState[Integer] = _
+  private var accumulationUserCount: ValueState[Integer] = _
 
 //  private var metric:(String,  String, Long, Integer) = _
 
   override def open(parameters: Configuration): Unit = {
-    val userCountDescriptor = new ValueStateDescriptor[Integer]("userCount", classOf[Integer], 0)  /// 要设置默认值0
-    userCount = getRuntimeContext.getState[Integer](userCountDescriptor)
+    val userCountDescriptor = new ValueStateDescriptor[Integer]("currentUserCount", classOf[Integer], 0)  /// 要设置默认值0
+    currentUserCount = getRuntimeContext.getState[Integer](userCountDescriptor)
+
+    val accumulationUserCountDescriptor = new ValueStateDescriptor[Integer]("accumulationUserCount", classOf[Integer], 0)  /// 要设置默认值0
+    accumulationUserCount = getRuntimeContext.getState[Integer](accumulationUserCountDescriptor)
   }
 
-  override def flatMap(value: MonitorRoomBean, out: Collector[(String,  String, Long, Integer)]): Unit = {
-    var userAmount = userCount.value
+  override def flatMap(value: MonitorRoomBean, out: Collector[(String,  String, Long, Integer,Integer)]): Unit = {
+    var currentUserAmount = currentUserCount.value
+    var accumulationUserAmount = accumulationUserCount.value()
 
     val roomId: String = value.roomId
     val userId: String = value.userId
     val time: Long = value.time
 
-//    println("roomId",roomId)
-//    println("userId",userId)
-//    println("userAmount",userAmount)
+//    metric = (roomId, userId, time, currentUserAmount)
+    out.collect((roomId, userId, time, currentUserAmount,accumulationUserAmount ))
 
-//    metric = (roomId, userId, time, userAmount)
-    out.collect((roomId, userId, time, userAmount))
-
-    if ( value.statusType == Constants.STATUS_TYPE_INIT ) {
-      println("roomId-",roomId, "-join")
-      userAmount += 1
+    if (value.statusType == Constants.STATUS_TYPE_INIT ) {
+      println("roomId", roomId , "userId", userId, "join")
+      currentUserAmount += 1
     }
     if ( value.statusType == Constants.STATUS_TYPE_LEAVE ) {
-      println("roomId-",roomId, "-leave")
-      userAmount -= 1
+      println("roomId",roomId, "userId", userId, "leave")
+      currentUserAmount -= 1
     }
-    this.userCount.update(userAmount)
+    accumulationUserAmount += 1
+
+    currentUserCount.update(currentUserAmount)
+    accumulationUserCount.update(currentUserAmount)
   }
-
-//  override def open(parameters: Configuration): Unit = {
-//    val mapDesc = new MapStateDescriptor[Long,AdlogBean]("StatisticalIndic", classOf[(Long)], classOf[(AdlogBean)] ) /// StatisticalIndic(0)
-//    mapState = getRuntimeContext().getMapState(mapDesc)
-//  }
-
-//  override def restoreState(list: util.List[(String, Long, Long, String, String)]): Unit = {
-//  }
-//
-//  override def snapshotState(l: Long, l1: Long): util.List[(String, Long, Long, String, String)] = {
-//          val list : util.List[(String, Long, Long, String, String)] = new util.ArrayList();
-//          list.add(metric)
-//          list
-//  }
 }
