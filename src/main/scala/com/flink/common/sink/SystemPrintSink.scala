f9f3949aa6aaa21a5ade299f0c4005e39e70465b
bruce.wang <www6v@126.com>
2019-09-19 10:59:00 +0800

    fix

--- src/main/scala/com/flink/common/sink/SystemPrintSink.scala
+++ src/main/scala/com/flink/common/sink/SystemPrintSink.scala
@@ -58,53 +58,30 @@ import org.apache.flink.configuration.Configuration
     prometheusPush.push(gaugeDelay, "delayOfUser")
   }
 
-  //    "prometheus-gateway.app.pre.urome.cn"
-  prometheusPush = new PushGateway(  PUSH_GATAWAY )
-
-  gaugeBr  = Gauge.build.name("biteRateOfTheUser" + "Test").
-    labelNames("userid" ).
-    //       labelNames("userid", "lostPre","frt","delay" ).
-    help("rtc monitor").register
-
-  gaugeLostPre = Gauge.build.name("lostPreOfTheUser"+ "Test").
-    //      labelNames("userid", "br", "frt", "delay").
-    labelNames("userid").
-    help("rtc monitor").register
-
-  gaugeFrt  = Gauge.build.name("frtOfTheUser"+ "Test").
-    //      labelNames("userid", "br","lostPre","delay" ).
-    labelNames("userid" ).
-    help("rtc monitor").register
-
-  gaugeDelay  = Gauge.build.name("delayOfTheUser"+ "Test").
-    //      labelNames("userid", "br", "lostPre", "frt").
-    labelNames("userid").
-    help("rtc monitor").register
-
   override def open( parameters:Configuration) {
 
-////    "prometheus-gateway.app.pre.urome.cn"
-//     prometheusPush = new PushGateway(  PUSH_GATAWAY )
+//    "prometheus-gateway.app.pre.urome.cn"
+     prometheusPush = new PushGateway(  PUSH_GATAWAY )
 
-//     gaugeBr  = Gauge.build.name("biteRateOfTheUser" + "Test").
-//      labelNames("userid" ).
-////       labelNames("userid", "lostPre","frt","delay" ).
-//      help("rtc monitor").register
-//
-//    gaugeLostPre = Gauge.build.name("lostPreOfTheUser"+ "Test").
-////      labelNames("userid", "br", "frt", "delay").
-//      labelNames("userid").
-//      help("rtc monitor").register
-//
-//    gaugeFrt  = Gauge.build.name("frtOfTheUser"+ "Test").
-////      labelNames("userid", "br","lostPre","delay" ).
-//      labelNames("userid" ).
-//      help("rtc monitor").register
-//
-//    gaugeDelay  = Gauge.build.name("delayOfTheUser"+ "Test").
-////      labelNames("userid", "br", "lostPre", "frt").
-//      labelNames("userid").
-//      help("rtc monitor").register
+     gaugeBr  = Gauge.build.name("biteRateOfTheUser").
+      labelNames("userid" ).
+//       labelNames("userid", "lostPre","frt","delay" ).
+      help("rtc monitor").register
+
+    gaugeLostPre = Gauge.build.name("lostPreOfTheUser").
+//      labelNames("userid", "br", "frt", "delay").
+      labelNames("userid").
+      help("rtc monitor").register
+
+    gaugeFrt  = Gauge.build.name("frtOfTheUser").
+//      labelNames("userid", "br","lostPre","delay" ).
+      labelNames("userid" ).
+      help("rtc monitor").register
+
+    gaugeDelay  = Gauge.build.name("delayOfTheUser").
+//      labelNames("userid", "br", "lostPre", "frt").
+      labelNames("userid").
+      help("rtc monitor").register
   }
 
   override def close(): Unit = {
