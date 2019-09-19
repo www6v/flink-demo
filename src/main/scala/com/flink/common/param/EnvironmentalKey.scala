cd4af7fd4b58ed228efbe09bc16ff12090938859
bruce.wang <www6v@126.com>
2019-09-19 11:44:01 +0800

    fix

--- src/main/scala/com/flink/common/param/EnvironmentalKey.scala
+++ src/main/scala/com/flink/common/param/EnvironmentalKey.scala
@@ -18,7 +18,9 @@ trait EnvironmentalKey {
   val TOPIC_JOIN_LEAVE  = "flink-rtc-log-join-leave"  /// fluentd
 
 
-  val PUSH_GATAWAY = "prometheus-gateway.sh.internal.pre.urtc.com.cn"  //
+//  val PUSH_GATAWAY = "prometheus-gateway.sh.internal.pre.urtc.com.cn"  //
+  val PUSH_GATAWAY = "prometheus-gateway.sh.pre.urtc.com.cn"  //
+  
 //  val PUSH_GATAWAY = "prometheus-gateway.sh.internal.prod.urtc.com.cn"  //
 //  val PUSH_GATAWAY = "prometheus-gateway.sh.prod.urtc.com.cn"
 
